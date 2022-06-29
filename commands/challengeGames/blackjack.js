470926d44c2b74427b52322ed97fe255c2d026f1
vidalatan <vidalatan@gmail.com>
2022-06-29 18:29:22 -0500

    Blackjack started

--- commands/challengeGames/blackjack.js
+++ commands/challengeGames/blackjack.js
@@ -1,6 +1,42 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+const SUITS = ['♦️','♥️','♣️','♠️'];
+const VALUES = 
+[
+  'A', 'J', 'Q', 'K',
+  '1', '2', '3', '4',
+  '5', '6', '7', '8',
+  '9', '10'
+]
+
+const DECK = SUITS.flatMap(suit => {
+  const set = []
+  for(let value of VALUES){
+    set.push(value+suit)
+  }
+  return set
+})
+
+function shuffle(){
+  
+}
+
+function createHand(isDealer=false){
+  
+}
+
 module.exports = {
   chg:'blj',
   play(ctx, target){
-    ctx.channel.send(`Blackjack started... well not yet because I haven\'t actually programmed it. But if I had, ${ctx.author} would be challenging ${target} to Blackjack`)
+    console.log(DECK);
+    const bljEmb = new MessageEmbed()
+      .setTitle('Blackjack')
+      .setDescription(`Dealer: ??`)
+      .addField('Deck', `${DECK}`, true)
+
+    ctx.channel.send({embeds: [bljEmb]})
+  },
+
+  interaction(ctx){
+
   }
 }
--- commands/challengeGames/holdem.js
+++ commands/challengeGames/holdem.js
@@ -2,5 +2,9 @@ module.exports = {
   chg:'hld',
   play(ctx, target){
     ctx.channel.send(`Texas hold'em started... well not yet because I haven\'t actually programmed it. But if I had, ${ctx.author} would be challenging ${target} to Texas hold'-em`)
+  },
+
+  interaction(ctx){
+    
   }
 }
