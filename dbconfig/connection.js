4c8577f6cc793a327439df391e4d62eb0b87f42d
Vidalatan <vidalatan@gmail.com>
2022-07-01 21:42:39 -0500

    Password hidden
    I often show the code working to others. I noticed at one point I kept
    flashing my mongodb atlas password.

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/jsbotDB';
-console.log(connectionString);
+console.log(connectionString.replace(connectionString.slice(connectionString.indexOf('root:')+5, connectionString.indexOf('@main')+1),'<*******>'));
 mongoose.connect(connectionString)
 
 
